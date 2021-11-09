aaacf759bb40604bf94c018d02f6ccee246aed86
moytyr9 <92784686+moytyr9@users.noreply.github.com>
2021-11-09 12:48:05 +0300

    Add files via upload

---
+++ КУзнецов Кирилл/09.11 Человечек/танцует.pas
@@ -0,0 +1,22 @@
+﻿program dotA2;
+   uses GraphABC;
+     begin
+       setpenwidth(3);
+       setpencolor(clblack);
+       line(30,50,60,60);
+       line(30,50,20,55);
+       line(60,60,80,50);
+       line(80,50,110,40);
+       circle(85,35,10);
+       line(110,40,120,60);
+       line(120,60,130,30);
+       line(130,30,145,25);
+       line(90,50,110,90);
+       line(90,95,130,80);
+       line(90,95,93,134);
+       line(93,134,80,150);
+       line(80,150,85,160);
+       line(130,80,150,65);
+       line(150,65,140,100);
+       line(140,100,155,95);
+       end.

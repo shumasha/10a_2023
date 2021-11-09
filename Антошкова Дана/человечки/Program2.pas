6354c902c72b5348b85c2956c93768657af1011f
Данютка <92784704+Danutkaa@users.noreply.github.com>
2021-11-09 12:52:06 +0300

    Add files via upload

---
+++ Антошкова Дана/человечки/Program2.pas
@@ -0,0 +1,21 @@
+program graphika;
+uses GraphABC;
+begin
+circle(100,40,10);
+FloodFill(100,40,clblack);
+line(92,55,115,45);
+line(120,40,135,30);
+line(140,28,150,20);
+line(85,55,70,45);
+line(70,41,60,20);
+line(100,50,110,90);
+line(90,95,130,85);
+line(128,85,150,110);
+line(150,113,153,125);
+line(154,123,163,115);
+line(90,98,98,120);
+line(94,122,80,135);
+line(80,138,90,140);
+SetPenColor(clblue);
+line(90,10,30,40);
+end.
---
+++ Антошкова Дана/человечки/Program6.pas
@@ -0,0 +1,20 @@
+program graphika5;
+uses graphabc;
+begin
+circle(90,20,10);
+FloodFill(90,20,clblack);
+line(80,30,50,50);
+line(50,50,55,80);
+line(55,80,25,70);
+line(25,70,22,80);
+line(50,50,75,60);
+line(75,60,65,80);
+line(65,80,75,80);
+line(80,30,80,40);
+line(80,40,85,60);
+line(87,60,90,80);
+line(90,80,95,80);
+line(80,30,90,50);
+line(90,50,100,30);
+line(100,30,108,26);
+end.

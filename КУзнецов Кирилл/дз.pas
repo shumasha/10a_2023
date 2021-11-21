b3da15f8e632e633498b8c9a457240e57ac3c926
moytyr9 <92784686+moytyr9@users.noreply.github.com>
2021-11-21 17:08:05 +0300

    Add files via upload

---
+++ КУзнецов Кирилл/дз.pas
@@ -0,0 +1,12 @@
+﻿program dkd1;
+  var bak,polbak,s,rashod:real;
+  begin
+  Writeln('Введите обьем бака в литрах');
+  read(bak);
+  polbak:=bak/2;
+  Writeln('Введите расход топлива на 100км');
+  read(rashod);
+  s:=rashod*polbak;
+  write('На половине бака можно проехать - ',s);
+  write(' км');
+  end.

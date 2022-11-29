665bdd489c686a02c5673ab78b32a861c6ff8c04
ZhenyaKrutoy <93984099+ZhenyaKrutoy@users.noreply.github.com>
2022-11-29 10:23:01 +0300

    Add files via upload

---
+++ Качанов Евгений/Задача1 Лист, ноябрь.py
@@ -0,0 +1,10 @@
+def NOD(x,y):
+    while x!=y:
+        if x>y:
+            x-=y
+        else:
+            y-=x
+    return x
+a = int(input("First number: "))
+b = int(input("Second number: "))
+print('NOD: ', (a*b)/NOD(a,b))
